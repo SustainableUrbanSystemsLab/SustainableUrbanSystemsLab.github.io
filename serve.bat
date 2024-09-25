c34c577b540918a6c3e5035cdc63e2bf9fce3f88
kastnerp <patrick.kastner@gmail.com>
2024-09-25 22:52:04 +0000

    Deploying to gh-pages from @ SustainableUrbanSystemsLab/SustainableUrbanSystemsLab.github.io@857489ea8920e1289d6fc2d3373cbc387829e403 🚀

--- serve.bat
+++ serve.bat
@@ -1,5 +1,5 @@
 REM Delete all running containers
 FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
 REM Serve jekyll in directory
-docker run --rm -v "%cd%:/srv/jekyll" -p "8080:8080" -it amirpourmand/al-folio:latest /bin/sh -c "bundle install && jekyll serve --host 0.0.0.0 --port 8080" 
+docker run --rm -v "%cd%:/srv/jekyll" -p "8080:8080" -it amirpourmand/al-folio:latest /bin/sh -c "bundle install && jekyll serve --host 0.0.0.0 --port 8080 --watch" 
 PAUSE
